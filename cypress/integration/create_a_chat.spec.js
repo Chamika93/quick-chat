81f7d42860d02cce9757faded5eae9e20d95d38d
ChamikaAnd <chamika.adikari@and.digital>
2022-02-28 18:21:34 +0530

    initialise app and add cypress

---
+++ cypress/integration/create_a_chat.spec.js
@@ -0,0 +1,13 @@
+describe('Create a new chat room', () => {
+    it('Navigate to the chat page', () => {
+        cy.visit('http://localhost:3000');
+
+        cy.get('[data-testid="mainText"]').contains('Create disposable chat quickly without a hassle.');
+
+        cy.get('[data-testid="name"]')
+           .type('Chamika');
+        
+        cy.get('[data-testid="createChatButton"]')
+           .click();
+    })
+})
